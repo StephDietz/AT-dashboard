a58622ffdc69f42ac6b1f1cc4068e3f26364c645
Stephanie Dietz <stephaniedietz@Stephanies-MacBook-Pro.local>
2019-10-23 17:56:41 -0500

    created tabs / added buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,30 @@ module.exports = {
       backgroundColor: {
         primary: '#2BB0ED',
         secondary: '#ffed4a',
-        dark: '#16191F',
-        accent: '#20232a',
-        light: '#2d2f35'
+        darkest: '#16191F',
+        dark: '#20232a',
+        chosen: '#2b2f38',
+        light: '#3b3b47',
+        lighter: '#393b43',
+        lightest: '#757a8a'
       },
       textColor: {
         primary: '#2BB0ED',
         secondary: '#ffed4a',
         dark: '#16191F',
         accent: '#20232a',
-        light: '#2d2f35'
+        light: '#2d2f35',
+        lighter: '#393b43',
+        lightest: '#757a8a'
       },
       borderColor: {
         primary: '#2BB0ED',
         secondary: '#ffed4a',
         dark: '#16191F',
         accent: '#20232a',
-        light: '#2d2f35'
+        light: '#2d2f35',
+        lighter: '#393b43',
+        lightest: '#757a8a'
       }
     }
   },
